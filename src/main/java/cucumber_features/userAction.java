542e7c60ea0c4010547e0a8c98529027efa18087
Hansong <yanghansong1996@live.com>
2019-10-03 21:22:55 -0400

    Cucumber classes and features

---
+++ src/main/java/cucumber_features/userAction.java
@@ -0,0 +1,55 @@
+package cucumber_features;
+import static org.junit.Assert.assertEquals;
+
+import comp4004A1.gameClient;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class userAction {
+	private boolean status = false;
+	
+	@Given("Moe enters the game and the game starts")
+	public void moe_enters_the_game() {
+		// The server is running
+	    // we have checked Moe enters the game
+	}
+
+	@When("Moe enters a {int} for action")
+	public void moe_enters_a_for_action(Integer int1) {
+	   status = gameClient.userActionChecker(int1);
+	   
+	}
+
+	@Then("I verify the true in step")
+	public void i_verify_the_success_in_step() {
+	    assertEquals(true, status);
+	}
+
+	@Given("Joe enters the game and the game starts")
+	public void joe_enters_the_game() {
+		// The server is running
+	    // we have checked Moe enters the game
+	}
+
+	@When("Joe enters a {int} for action")
+	public void joe_enters_a_for_action(Integer int1) {
+		status = gameClient.userActionChecker(int1);
+	}
+
+	@Given("Sam enters the game and the game starts")
+	public void sam_enters_the_game() {
+		// The server is running
+	    // we have checked Moe enters the game
+	}
+
+	@When("Sam enters a {int} for action")
+	public void sam_enters_a_for_action(Integer int1) {
+		status = gameClient.userActionChecker(int1);
+	}
+
+	@Then("I verify the false in step")
+	public void i_verify_the_Fail_in_step() {
+		assertEquals(false, status);
+	}
+}
---
+++ src/test/java/cucumber_Test/userAction.feature
@@ -0,0 +1,14 @@
+@UserAction
+Feature: Test the user action
+
+  @tag1
+  Scenario Outline: A player enters a number for user action
+    Given <name> enters the game and the game starts
+    When <name> enters a <value> for action
+    Then I verify the <status> in step
+
+    Examples: 
+      | name | value | status |
+      | Moe  |     1 | true   |
+      | Joe  |     2 | true   |
+      | Sam  |     4 | false  |
